9959b31572b8d1f5a1f636e564f1fe48d79d143f
Adam Wright <awright18@gmail.com>
2017-02-09 21:27:20 -0600

    Adding Cross Platform Projects

---
+++ FSharpCrossPlatformXamarinForms/FSharpCrossPlatformXamarinForms.Droid/MainActivity.fs
@@ -0,0 +1,32 @@
+﻿namespace FSharpCrossPlatformXamarinForms.Droid
+
+open System
+
+open Android.App
+open Android.Content
+open Android.Content.PM
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Xamarin.Forms
+open Xamarin.Forms.Platform.Android
+open FSharpCrossPlatformXamarinForms
+
+type Resources = FSharpCrossPlatformXamarinForms.Droid.Resource
+
+[<Activity(Label = "XamarinFormsFSharpDemo.Droid", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+type MainActivity () =
+    inherit FormsAppCompatActivity()
+
+    override this.OnCreate (bundle) =
+
+        FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
+
+        FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
+
+        base.OnCreate (bundle)
+
+        Forms.Init(this,bundle)
+
+        base.LoadApplication(new App())
---
+++ FSharpCrossPlatformXamarinForms/FSharpCrossPlatformXamarinForms.iOS/AppDelegate.fs
@@ -0,0 +1,21 @@
+namespace FSharpCrossPlatformXamarinForms.iOS
+
+open System
+open UIKit
+open Foundation
+open Xamarin.Forms
+open Xamarin.Forms.Platform.iOS
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit FormsApplicationDelegate ()
+
+    let window = new UIWindow (UIScreen.MainScreen.Bounds)
+
+    // This method is invoked when the application is ready to run.
+    override this.FinishedLaunching (app, options) =
+        // If you have defined a root view controller, set it here:
+        // window.RootViewController <- new MyViewController ()
+       do Forms.Init()
+
+       base.FinishedLaunching(app, options)
